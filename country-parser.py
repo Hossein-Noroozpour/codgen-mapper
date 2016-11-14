b26581c488b4a24c649434e739c1c66f8f0a942f
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2016-11-14 15:42:23 +0330

    converters in progress

---
+++ country-parser.py
@@ -0,0 +1,7 @@
+fi = open("C:\\Users\\h.norouzpour\\PycharmProjects\\codgen-mapper\\countries.txt")
+fo = open("C:\\Users\\h.norouzpour\\PycharmProjects\\codgen-mapper\\countries.py", "w")
+fo.write("countries = dict()\n")
+for l in fi:
+    s = l.strip()
+    s = s.split("\t")
+    fo.write("countries[" + s[1] + "] = '" + s[0] + "'\n")
