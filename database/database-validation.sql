ee5036259ee5818f40ff607f5f22a79de23f4f23
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-01-30 12:52:09 +0330

    validator for employee

--- database/database-validation.sql
+++ database/database-validation.sql
@@ -141,12 +141,17 @@ from Eris.dbo.List
 where KarkonanNo is null or KarkonanNo = 0
 
 
+
+
+
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id As ErrorListID, Ep.Id AS ErrorEmpID
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(continous_taxable_income_year_to_previous_month IS NULL
@@ -155,14 +160,18 @@ WHERE
 		SELECT SUM(IIF(outstanding_payments_received IS NULL, 0, outstanding_payments_received))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) + (
 		SELECT SUM(IIF(sum_ongoing_gross_salary IS NULL, 0, sum_ongoing_gross_salary))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -173,11 +182,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(housing_expenses_previous_month IS NULL
@@ -186,8 +197,10 @@ WHERE
 		SELECT SUM(IIF(housing_expense_salary IS NULL, 0, housing_expense_salary))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -198,11 +211,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(vehicle_expenses_previous_month IS NULL
@@ -211,8 +226,10 @@ WHERE
 		SELECT SUM(IIF(vehicle_expense_salary IS NULL, 0, vehicle_expense_salary))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -223,11 +240,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(non_cash_year_to_last_month IS NULL
@@ -236,8 +255,10 @@ WHERE
 		SELECT SUM(IIF(non_cash_payments IS NULL, 0, non_cash_payments))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -248,11 +269,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(sum_exemptions_deductions_to_last_month IS NULL
@@ -261,8 +284,10 @@ WHERE
 		SELECT SUM(IIF(total_exemption_deduction IS NULL, 0, total_exemption_deduction))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -273,11 +298,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(continous_tax_year_to_previous_month IS NULL
@@ -286,8 +313,10 @@ WHERE
 		SELECT SUM(IIF(outcome_tax_table IS NULL, 0, outcome_tax_table))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -298,11 +327,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(total_uncontinous_income_year_up_previous_month IS NULL
@@ -311,8 +342,10 @@ WHERE
 		SELECT SUM(IIF(total_uncontinous_payments IS NULL, 0, total_uncontinous_payments))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -323,11 +356,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(non_cash_uncontinous_benefit_year_up_previous_month IS NULL
@@ -336,8 +371,10 @@ WHERE
 		SELECT SUM(IIF(non_cash_uncontinous_benefit_current_month IS NULL, 0, non_cash_uncontinous_benefit_current_month))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -348,11 +385,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(tax_uncontinous_income_the_year_up_previous_month IS NULL
@@ -361,32 +400,10 @@ WHERE
 		SELECT SUM(IIF(total_tax_uncontinous_income IS NULL, 0, total_tax_uncontinous_income))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
-	) != 0
-	AND Li.Month != 1
-ORDER BY
-	NationalCode
-
-
-
-
-SELECT
-	DISTINCT NationalCode
-FROM
-	Eris.dbo.Salary
-	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
-	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
-	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
-WHERE
-	(tax_uncontinous_income_the_year_up_previous_month IS NULL
-	OR tax_uncontinous_income_the_year_up_previous_month = 0)
-	AND (
-		SELECT SUM(IIF(total_tax_uncontinous_income IS NULL, 0, total_tax_uncontinous_income))
-		FROM Eris.dbo.Salary
-			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
-		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -397,11 +414,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(total_bonus_start_year_to_previous_month IS NULL
@@ -410,8 +429,10 @@ WHERE
 		SELECT SUM(IIF(year_end_bonus_new_year IS NULL, 0, year_end_bonus_new_year))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -422,11 +443,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(tax_bonus_from_first_year_to_month_before IS NULL
@@ -435,8 +458,10 @@ WHERE
 		SELECT SUM(IIF(tax_continous_uncontinous_income IS NULL, 0, tax_continous_uncontinous_income))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -447,11 +472,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(total_previous_compensation_unused_leave_from_beginig_year_up IS NULL
@@ -460,8 +487,10 @@ WHERE
 		SELECT SUM(IIF(unused_leave_payments_amount IS NULL, 0, unused_leave_payments_amount))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
@@ -472,11 +501,13 @@ ORDER BY
 
 
 SELECT
-	DISTINCT NationalCode
+	DISTINCT NationalCode, Li.Id
 FROM
 	Eris.dbo.Salary
 	JOIN Eris.dbo.List AS Li ON Eris.dbo.Salary.ListId = Li.Id
 	JOIN Eris.dbo.Employer AS Em ON Em.Id = Li.UserId
+	JOIN Eris.dbo.CompEmp AS Cm ON Cm.UserId = Em.Id
+	JOIN Eris.dbo.Employee AS Ep ON Cm.EmployeeId = Ep.Id
 	JOIN ErisHelper.dbo.EmployerFilter ON ErisHelper.dbo.EmployerFilter.tin = Em.NationalCode
 WHERE
 	(annual_tax_up_month_before_compensation_unused_leave IS NULL
@@ -485,8 +516,10 @@ WHERE
 		SELECT SUM(IIF(tax_continous_uncontinous_income_bonus IS NULL, 0, tax_continous_uncontinous_income_bonus))
 		FROM Eris.dbo.Salary
 			JOIN Eris.dbo.List ON Eris.dbo.Salary.ListId = Eris.dbo.List.Id
+			JOIN Eris.dbo.CompEmp ON Eris.dbo.List.UserId = Eris.dbo.CompEmp.UserId
+			JOIN Eris.dbo.Employee ON Eris.dbo.Employee.Id = Eris.dbo.CompEmp.EmployeeId
 		WHERE
-			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId
+			Li.Month > Eris.dbo.List.Month AND Em.Id = Eris.dbo.List.UserId AND Eris.dbo.Employee.Id = Ep.Id
 	) != 0
 	AND Li.Month != 1
 ORDER BY
