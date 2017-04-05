e7e3afd88aa69993c772d36bce0969b9133358e4
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-04-05 14:04:54 +0430

    validator documented

--- database/database-validation.sql
+++ database/database-validation.sql
@@ -1,149 +1,138 @@
 use Eris;
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as InsuranceId_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where InsuranceId is NULL or InsuranceId = 0;
-
-
---select count(Employee.Id) as WorkPlaceStatus_IS_NULL_EmployeeIds
---from Salary
---join List on Salary.ListId=List.Id
---join CompEmp on CompEmp.Id=Salary.CompEmpId
---join Employee on Employee.Id=CompEmp.EmployeeId
---where CompEmp.WorkPlaceStatus is NULL or CompEmp.WorkPlaceStatus = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
+select count(Employee.Id) as WorkPlaceStatus_IS_NULL_EmployeeIds
+from Salary
+join List on Salary.ListId=List.Id
+join CompEmp on CompEmp.Id=Salary.CompEmpId
+join Employee on Employee.Id=CompEmp.EmployeeId
+where CompEmp.WorkPlaceStatus is NULL or CompEmp.WorkPlaceStatus = 0;
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as EmploymentTypeId_Is_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where EmploymentTypeId is NULL or EmploymentTypeId = 0;
-
-
---select count(Employee.Id) as MoafiatId_Is_NULL_EmployeeIds
---from Salary
---join List on Salary.ListId=List.Id
---join CompEmp on CompEmp.Id=Salary.CompEmpId
---join Employee on Employee.Id=CompEmp.EmployeeId
---where MoafiatId is NULL or MoafiatId = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
+select count(Employee.Id) as MoafiatId_Is_NULL_EmployeeIds
+from Salary
+join List on Salary.ListId=List.Id
+join CompEmp on CompEmp.Id=Salary.CompEmpId
+join Employee on Employee.Id=CompEmp.EmployeeId
+where EmployeeMoafiatId is NULL or MoafiatId = 0;
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as NumberRealWorkedMonth_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where NumberRealWorkedMonth is NULL or NumberRealWorkedMonth = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as PayType_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where PayType is NULL or PayType = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as ExchangeRate_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where ExchangeRate is NULL or ExchangeRate = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as HouseId_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where HouseId is NULL or HouseId = 0;
-
-
+-- According to Email {time: "Tuesday, November 29, 2016 2:25 PM", subject: "FRM32 NULL FIELDS"}
+-- This field can not be null
 select count(Employee.Id) as carId_IS_NULL_EmployeeIds
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
 where carId is NULL or carId = 0;
-
-
-
+-- For checking the conformity of the data to the enums
 select distinct EducationCertificate
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
 select distinct NationalityId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
 select distinct InsuranceId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
 select distinct EmploymentTypeId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
---select distinct CompEmp.WorkPlaceStatus
---from Salary
---join List on Salary.ListId=List.Id
---join CompEmp on CompEmp.Id=Salary.CompEmpId
---join Employee on Employee.Id=CompEmp.EmployeeId
-
-
---select distinct MoafiatId
---from Salary
---join List on Salary.ListId=List.Id
---join CompEmp on CompEmp.Id=Salary.CompEmpId
---join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
+select distinct CompEmp.WorkPlaceStatus
+from Salary
+join List on Salary.ListId=List.Id
+join CompEmp on CompEmp.Id=Salary.CompEmpId
+join Employee on Employee.Id=CompEmp.EmployeeId
+-- For checking the conformity of the data to the enums
+select distinct Employee.MoafiatId
+from Salary
+join List on Salary.ListId=List.Id
+join CompEmp on CompEmp.Id=Salary.CompEmpId
+join Employee on Employee.Id=CompEmp.EmployeeId
+-- For checking the conformity of the data to the enums
 select distinct HouseId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
 select distinct CarId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- For checking the conformity of the data to the enums
 select distinct PaymentId
 from Salary
 join List on Salary.ListId=List.Id
 join CompEmp on CompEmp.Id=Salary.CompEmpId
 join Employee on Employee.Id=CompEmp.EmployeeId
-
-
+-- This is whatever impossible to have a employer with no employee
 select count(Id) as KarkonanNoIsZeroOrNull
 from Eris.dbo.List
 where KarkonanNo is null or KarkonanNo = 0
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id As ErrorListID, Ep.Id AS ErrorEmpID
 FROM
@@ -183,11 +172,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -222,11 +210,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -262,11 +249,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -302,11 +288,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -342,11 +327,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id, Li.KarkonanNo, Ep.Id, NumberRealWorkedMonth
 FROM
@@ -382,11 +366,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -422,11 +405,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -462,11 +444,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -502,11 +483,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -539,11 +519,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -576,11 +555,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id
 FROM
@@ -615,11 +593,10 @@ WHERE
 	)
 ORDER BY
 	NationalCode
-
-
-
-
-
+-- According to Email: {time: "Sunday, January 22, 2017 10:28 AM", subject: "FRM32-1395"} and spec
+-- This field must be total amount of other field till previous month
+-- Note, this query does not check that the aggregated field is exactly sum of all previous once, but it is checking for
+-- being null, if this query show any results, this mean there is a problem in this spec rule
 SELECT
 	DISTINCT Em.NationalCode, Li.Id, Ep.Id
 FROM
