5c2e3e00df7ea16fb50c3d372873b7b22adddd94
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-04-26 16:05:47 +0430

    some new queries

--- database/database-restore.sql
+++ database/database-restore.sql
@@ -1,5 +1,8 @@
+USE ErisHelper
+DROP DATABASE Eris
+-- First check the file names in here
+RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/backup/Eris.bak'
 RESTORE DATABASE Eris
-FROM DISK = '/var/opt/mssql/backup/Eris-14-01-2017.bak'
-WITH MOVE 'Eris' TO '/var/opt/mssql/data/Eris_Data.mdf',
+FROM DISK = '/var/opt/mssql/backup/Eris.bak'
+WITH MOVE 'Eris_Data' TO '/var/opt/mssql/data/Eris_Data.mdf',
 MOVE 'Eris_Log' TO '/var/opt/mssql/data/Eris_Log.ldf'
-GO
--- database/tax-payer-selection.sql
+++ database/tax-payer-selection.sql
@@ -1,32 +1,66 @@
-USE Eris;
-SELECT List.Id, List.UserId, Employer.NationalCode
+SELECT Li.Id, Li.UserId, Em.NationalCode
 FROM
-    List
-    JOIN Employer ON List.UserId = Employer.Id
+    Eris.dbo.List AS Li
+    JOIN Eris.dbo.Employer AS Em ON Li.UserId = Em.Id
 WHERE
-	((List.RoznameDate / 100) % 100) - ((List.Date_Ghabz/ 100) % 100) < 2
-	and List.Date_Ghabz IS NOT NULL
-	AND List.PaidDate   IS NOT NULL
-	AND List.RoznameDate IS NOT NULL and list.hozeh like '3625%'
-	AND List.UserId in (
-		SELECT emp.Id
+	((Li.RoznameDate / 100) % 100) - ((Li.Date_Ghabz/ 100) % 100) < 2
+	AND Li.Date_Ghabz IS NOT NULL
+	AND Li.PaidDate   IS NOT NULL
+	AND Li.RoznameDate IS NOT NULL
+	-- AND Li.hozeh like '3625%'
+	AND Li.UserId IN (
+		SELECT Emp.Id
 		FROM
-		    Employer as emp
+		    Eris.dbo.Employer AS Emp
 		WHERE
 			(
-				select
-					count(list.Id)
-				from
-					List
-				where
-					list.UserId = emp.Id
-					and ((List.RoznameDate / 100) % 100) - ((List.Date_Ghabz/ 100) % 100) < 2
-					and List.Date_Ghabz IS NOT NULL
-					AND List.PaidDate   IS NOT NULL
-					AND List.RoznameDate IS NOT NULL and list.hozeh like '3625%') = 6
-			and (select count(distinct list.Month) from List where List.UserId = emp.Id and list.hozeh like '3625%') = 6
-			and (select count(list.Id) from List where List.UserId = emp.Id and list.hozeh like '3625%') = 6
-			and (select max(list.month) from List where List.UserId = emp.Id and list.hozeh like '3625%') = 6
+				SELECT
+					COUNT(Lis.Id)
+				FROM
+					Eris.dbo.List AS Lis
+				WHERE
+					Lis.UserId = Emp.Id
+					AND ((Lis.RoznameDate / 100) % 100) - ((Lis.Date_Ghabz/ 100) % 100) < 2
+					AND Lis.Date_Ghabz  IS NOT NULL
+					AND Lis.PaidDate    IS NOT NULL
+					AND Lis.RoznameDate IS NOT NULL
+					-- AND Lis.hozeh like '3625%'
+			) = 9
+			AND (
+				SELECT COUNT(DISTINCT Li3.Month)
+				FROM Eris.dbo.List AS Li3
+				WHERE
+					Li3.UserId = Emp.Id
+					--AND Li3.hozeh LIKE '3625%'
+			) = 9
+			AND (
+				SELECT COUNT(Li3.Id)
+				FROM Eris.dbo.List AS Li3
+				WHERE
+					Li3.UserId = Emp.Id
+					--AND Li3.hozeh LIKE '3625%'
+			) = 9
+			AND (
+				SELECT MAX(Li3.Month)
+				FROM Eris.dbo.List AS Li3
+				WHERE
+					Li3.UserId = Emp.Id
+					--AND Li3.hozeh LIKE '3625%'
+			) = 9
 	)
 ORDER BY
-	List.UserId, List.Id
+	Li.UserId, Li.Id
+
+
+SELECT
+	Li.RoznameDate,
+	Li.Date_Ghabz,
+	Li.PaidDate,
+	Li.Month,
+	Em.NationalCode
+FROM
+    Eris.dbo.List AS Li
+    JOIN Eris.dbo.Employer AS Em ON Li.UserId = Em.Id
+ORDER BY
+	Em.NationalCode,
+	Li.Month
